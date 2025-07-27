e3aa0ab48ab4990658795fe3e09aaf87184dc12a
erezkalifa <erezkalifa@gmail.com>
2025-07-27 20:07:13 +0300

    fixed sticky widget

--- src/utils/api.utils.js
+++ src/utils/api.utils.js
@@ -66,3 +66,12 @@ export function getErrorMessage(error) {
 
   return "An unexpected error occurred";
 }
+
+export function removeEmojis(text) {
+  return text
+    .replace(
+      /[\u{1F300}-\u{1F9FF}]|[\u{1F600}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]/gu,
+      ""
+    )
+    .trim();
+}
